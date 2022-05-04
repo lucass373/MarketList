8bdf46476c4b334e392bf79acb5c060cfe9df942
lucass373 <78789490+lucass373@users.noreply.github.com>
2022-05-04 08:05:25 -0300

    .

--- firebase/firebase.tsx
+++ firebase/firebase.tsx
@@ -3,19 +3,7 @@ import { getDatabase, ref } from "firebase/database";
 
 const firebaseConfig = {
 
-  apiKey: "AIzaSyC1YeYP7tFNPofX1GP_S9PSjxqNUaMQuIk",
-
-  authDomain: "tasks-212cc.firebaseapp.com",
-
-  databaseURL: "https://tasks-212cc-default-rtdb.firebaseio.com",
-
-  projectId: "tasks-212cc",
-
-  storageBucket: "tasks-212cc.appspot.com",
-
-  messagingSenderId: "741148865055",
-
-  appId: "1:741148865055:web:b7b10fcdd323595facdb2a"
+  //your API key
 
 };
 const initFire = initializeApp(firebaseConfig);
